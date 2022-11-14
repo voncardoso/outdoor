7b3a905757ab24ede6ccaad9b0602173d1977129
voncardoso <vonharrison26@gmail.com>
2022-11-14 20:13:36 -0300

    header finalizado

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import teste from "../../../public/assets/faleconosco.svg";
+import styles from "./Header.module.css";
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link href="#" className={styles.link}>
+          <Image
+            className={styles.IconFaleConosco}
+            src={teste}
+            alt="Fale com nosco"
+          />
+          <p className={styles.text}>Fale Conosco</p>
+        </Link>
+      </nav>
+    </header>
+  );
+}
